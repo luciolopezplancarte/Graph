b931b97ea571ac4664a34853d922433014c168c8
Lucio Plancarte <lucio.lopezplancarte@enmu.edu>
2025-11-14 13:03:37 -0700

    Finished FlightManager class methods. Test program failed because of f-string and quotation marks. Adding single quotes solved the issue. Using onlinegdb.com that interpreter did not care about the double quotes. and ran the program fine. Program otherwise working as expected.

--- FlightManager.py
+++ FlightManager.py
@@ -14,24 +14,29 @@ class FlightManager(Graph):
 
         Use any of the inherited methods.
         """
+        super().__init__(file)
+        self.read_graph_from_file()
 
     def show_destinations(self, city):
         """
         Returns all the flight destinations starting at a given city.
         Use any of the inherited methods.
         """
+        return self.get_connections(city)
 
     def can_travel_to(self,source, destination):
         """
         Returns True or False if there is a path between cities
         Use any of the inheried methods
         """
+        return self.search(source,destination)
 
     def most_busy(self):
         """
         Returns the most busy cities
         Use any of the inherited methods
         """
+        return self.highest_degree()
 
 
 
--- testFlight.py
+++ testFlight.py
@@ -1,7 +1,9 @@
 from FlightManager import FlightManager
 
 FM = FlightManager('airports.txt')
-print(f"London's destinations: {FM.show_destinations("London")}")
-print(f"Can a person fly from New York to Rome? {FM.can_travel_to("New York", "Rome")")
-print(f"Can a person fly from New York to Mexico? {FM.can_travel_to("New York", "Mexico")")
-print(f"Can a person fly from New York to Rome? {FM.most_busy()}")
+
+print(f"London's destinations: {FM.show_destinations('London')}")
+print(f"Can a person fly from New York to Rome? {FM.can_travel_to('New York','Rome')} ")
+print(f"Can a person fly from New York to Mexico? {FM.can_travel_to('New York','Mexico')} ")
+print(f"Which are the most busy airports? {FM.most_busy()} ")
+
